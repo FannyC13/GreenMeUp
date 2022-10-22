5f13bc448dfdf9522617cb3d032300fa92021d9a
Fanny Chang <fchang1311@gmail.com>
2022-10-22 22:25:35 +0200

    deleting things

--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const plantsRoute = require('./plants')
-router.use('/plants', plantsRoute);
-const clientRoute = require('./client')
-router.use('/client', clientRoute);
-
-module.exports = router;
