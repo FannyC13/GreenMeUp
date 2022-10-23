8c148c14c8bbc3aa48dd065feacbb0e81283cc81
Fanny Chang <fchang1311@gmail.com>
2022-10-23 06:30:52 +0200

    index.js

--- index.js
+++ index.js
@@ -2,25 +2,29 @@ const express = require('express');
 const app = express();
 const apiRoute = require('./routes/api')
 app.use(express.json());
+const path = require('path')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use('/api', apiRoute)
+
+app.use('/api',apiRoute)
 app.use(express.static('public'))
-app.get('/', function (req, res) {
+app.get('/',function(req,res){
     res.sendFile(__dirname + '/public/html/Login.html')
 })
 
-app.get('/Home', function (req, res) {
+app.get('/Home',function(req,res){
     res.sendFile(__dirname + '/public/html/Home.html')
 })
 
-app.get('/create-account.html', function (req, res) {
+app.get('/create-account.html',function(req,res){
     res.sendFile(__dirname + '/public/html/create-account.html')
 })
 
-app.get('/Profile.html', function (req, res) {
+app.get('/Profile.html',function(req,res){
     res.sendFile(__dirname + '/public/html/Profile.html')
 })
 
-app.listen('3000', () => {
+app.listen('3000',()=>{
     console.log('Server started at port 3000')
 })
