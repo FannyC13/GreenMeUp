54b304e5d3368c14d34def22a0f60511c447d98e
Paul Machard <paul.machard@efrei.net>
2022-10-27 05:01:26 +0200

    nav barre + filtre lier

--- public/js/queries/plantsQueries.js
+++ public/js/queries/plantsQueries.js
@@ -13,8 +13,13 @@ const selectAllPlants = async () => {
 
 const selectPlants = async (type, priceMin, priceMax) => {
     try{
-        const selectQuery = await db('plants').where({type : type}).whereBetween('price', [priceMin, priceMax]).select();
-        return selectQuery
+        if (type == "All types"){
+            const selectQuery1 = await db('plants').whereBetween('price', [priceMin, priceMax]).select();
+            return selectQuery1
+        }else{
+            const selectQuery = await db('plants').where({type : type}).whereBetween('price', [priceMin, priceMax]).select();
+            return selectQuery
+        }
     }catch(e){
         console.log("Error with SQL database")
         return null;
@@ -34,4 +39,3 @@ const searchPlant = async (name) => {
 exports.selectAllPlants = selectAllPlants;
 exports.selectPlants = selectPlants;
 exports.searchPlant = searchPlant;
-
--- routes/api/client.js
+++ routes/api/client.js
@@ -4,130 +4,127 @@ const db = require('../../database');
 const clients = require('../../public/js/queries/clientQueries')
 const plantsQuery = require('../../public/js/queries/plantsQueries')
 const bcrypt = require('bcrypt');
-router.use(express.urlencoded({extended:false}))
+router.use(express.urlencoded({ extended: false }))
 var userdata = null;
 
-router.get('/', function(req,res){
-    db.select().from('client').orderBy('mail').then(function(data){
+router.get('/', function (req, res) {
+    db.select().from('client').orderBy('mail').then(function (data) {
         res.send(data);
-});
+    });
 });
 
-router.post('/createUser', async (req,res) =>{
-    try{
+router.post('/createUser', async (req, res) => {
+    try {
         const hashedPasswords = await bcrypt.hash(req.body.password, 10)
         const create = clients.createUser(req.body.lastname, req.body.firstname, req.body.mail, hashedPasswords)
         create.then(user => {
-            if(user.status === "success"){
+            if (user.status === "success") {
                 res.redirect('/');
-            }else{
+            } else {
                 res.json(user)
             }
         }
-        )}catch{
-            res.status(500).send()
-        }
-    })
+        )
+    } catch {
+        res.status(500).send()
+    }
+})
 
-router.get('/Profile', function(req,res){
+router.get('/Profile', function (req, res) {
     res.render('Profile', userdata)
 })
 
-router.post('/Profile', function(req,res){
+router.post('/Profile', function (req, res) {
     const user = clients.SearchUser(req.body.mail)
-    try{
-        if (bcrypt.compare(req.body.password, user.password)){
+    try {
+        if (bcrypt.compare(req.body.password, user.password)) {
             user.then(user => {
-                if(user.status === "success"){
+                if (user.status === "success") {
                     res.render('Profile', user.data[0]);
-                }else{
+                } else {
                     res.json(user)
                 }
-            } )
+            })
             return done(null, user)
         }
-        else{
-            return done(null, false, {message : 'Password incorrect'})
+        else {
+            return done(null, false, { message: 'Password incorrect' })
         }
     }
-    catch(e){
+    catch (e) {
         return done(e)
     }
-    
+
 })
-
-router.post('/HomeSearch', function(req,res){
-    const select = plantsQuery.searchPlant(req.body.barreSearch);
-    select.then(plants => {
-        res.render('Home', {client: userdata, plants: plants});
-    });
-});
-
 
 //put is idempotent so you are editing the actual data
 
-router.post('/UpdateUser', async (req,res)=>{   
-    try{
-        const hashedPasswords =  await bcrypt.hash(req.body.password, 10)
-        const update = clients.updateUser(req.body.lastname,req.body.firstname,req.body.mail, hashedPasswords)
-        update.then(user =>{
-            updateUserData(req.body.lastname,req.body.firstname,req.body.mail,req.body.password)
-            if(user.success === true){
+router.post('/UpdateUser', async (req, res) => {
+    try {
+        const hashedPasswords = await bcrypt.hash(req.body.password, 10)
+        const update = clients.updateUser(req.body.lastname, req.body.firstname, req.body.mail, hashedPasswords)
+        update.then(user => {
+            updateUserData(req.body.lastname, req.body.firstname, req.body.mail, req.body.password)
+            if (user.success === true) {
                 res.render('Profile', userdata)
-            }else{
+            } else {
                 res.send("Error 404 not found")
             }
         }
-        )}catch{
-            res.status(500).send()
-        }
+        )
+    } catch {
+        res.status(500).send()
+    }
+})
 
-   
-        
+router.post('/', function (req, res) {
+    if (req.body.barreSearch) {
+        const select = plantsQuery.searchPlant(req.body.barreSearch);
+        select.then(plants => {
+            res.render('Home', { client: userdata, plants: plants });
+        });
+    }else if (req.body.typePlant) {
+            const select = plantsQuery.selectPlants(req.body.typePlant, req.body.pmin, req.body.pmax);
+            select.then(plants => {
+                res.render('Home', { client: userdata, plants: plants });
+            });
+        } else {
+            const Login = clients.SearchUser(req.body.mail, req.body.password);
+            const select = plantsQuery.selectAllPlants();
+            Login.then(user => {
+                if (user.status === "success") {
+                    select.then(plants => {
+                        data = user.data;
+                        userdata = data[0];
+                        userdata.password = req.body.password;
+                        res.render('Home', { client: data[0], plants: plants });
+                    });
+
+                } else {
+                    res.redirect('/')
+                }
+            })
+        }
     })
 
-router.post('/Homefilter', function(req, res){
-    const select = plantsQuery.selectPlants(req.body.typePlant, req.body.pmin, req.body.pmax);
+
+router.get('/home', function (req, res) {
+    const select = plantsQuery.selectAllPlants();
     select.then(plants => {
-        res.render('Home', {client: userdata, plants: plants});
+        res.render('Home', { plants: plants });
     });
-});
-
-router.post('/', function(req,res){
-    const Login = clients.SearchUser(req.body.mail,req.body.password);
-    const select = plantsQuery.selectAllPlants();
-    Login.then(user => {
-        if(user.status === "success"){
-            select.then(plants => {
-                data = user.data;
-                userdata = data[0];
-                userdata.password = req.body.password;
-                res.render('Home', {client: data[0], plants: plants});
-                });
-
-        }else{
-            res.redirect('/')
-        }
-    } )
-})
-
-router.get('/home', function(req,res){
-    const select = plantsQuery.selectAllPlants();
-        select.then(plants => {
-                res.render('Home', {plants: plants});
-                });
 })
 
 
 
-router.post('/DeleteClient', async (req,res) => {
-    const del = clients.DeleteUser( req.body.mail)
-    del.then(() =>{
+router.post('/DeleteClient', async (req, res) => {
+    const del = clients.DeleteUser(req.body.mail)
+    del.then(() => {
         res.redirect('/')
     })
 })
 
-function updateUserData(lastname,firstname,mail,password){
+function updateUserData(lastname, firstname, mail, password) {
     userdata.lastname = lastname
     userdata.firstname = firstname
     userdata.mail = mail
