3cf5edf56bb4990974b6454c58a04a99bbb33f78
ArthB94 <arthur.billebaut@efrei.net>
2022-10-28 10:32:24 +0200

    reparation login et d'autres petits trucs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const apiRoute = require('./routes/api')
 app.use(express.json());
 const path = require('path');
 var userdata = null;
+var plants = null;
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -18,8 +19,13 @@ app.get('/',function(req,res){
 app.get('/create-account',function(req,res){
     res.render('create-account')
 })
+app.get('*', async (req, res) => {
+    res.render('404')
+});
 
 app.listen('1010',()=>{
     console.log('Server started at port 1010')
 })
+
+
 
